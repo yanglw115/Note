6b3fa0510f0e22f686a978900cc0cff79ad4276c
yanglw115 <357969275@qq.com>
2024-12-28 15:13:52 +0800

    Create getOfflineInstall.bat

---
+++ getOfflineInstall.bat
@@ -0,0 +1,3 @@
+.\vs_Enterprise.exe --layout "D:\vs2022-offline\vs2022InstallOffline" --add Microsoft.VisualStudio.Workload.CoreEditor --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional --add  Microsoft.VisualStudio.Workload.NativeCrossPlat   --includeRecommended --includeOptional --add Microsoft.VisualStudio.Workload.VisualStudioExtension  --includeRecommended --includeOptional
+
+pause 
